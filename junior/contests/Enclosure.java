69bcfe0bc66bb2229041351e59daf8a77b43463f
Shiyuan Huang <19438967+Grey16@users.noreply.github.com>
2018-02-08 21:51:23 -0500

    Create Enclosure.java

---
+++ junior/contests/Enclosure.java
@@ -0,0 +1,15 @@
+/*
+    determine which symbol is missing
+    determine if it's a left or right
+    if right
+        Go past all left brackets + lower symbols (Ex ')' is lower than ']') 
+        LOOP
+            Record index in an ArrayList
+            Go right to another place
+    if left
+        Go past all higher symbols (Ex '{' is higher than '[')
+        LOOP
+            Record index in ArrayList
+            Go right to another place until a lower symbol is reached
+    SINGLE INTEGERS ARE NEVER ENCLOSED!
+*/
