5951cf8eb9ff720683eaac2f4897993b0209e808
Shiyuan Huang <19438967+Grey16@users.noreply.github.com>
2018-02-16 13:42:00 -0500

    Update Enclosure.java

--- junior/contests/Enclosure.java
+++ junior/contests/Enclosure.java
@@ -1,1 +1,16 @@
 //Java version of the Python program submitted for Contest 2
+import java.util.*;
+
+public class Enclosure {
+    public static void main(String[] args) {
+        for(int i = 0; i < 5; i++) {
+            ArrayList<String> enclosures = new ArrayList<String>();
+            Scanner in = new Scanner(System.in);
+            System.out.print("Enter the mathematical expression without any spaces: ");
+            String expression = in.nextLine();
+            if(expression.indexOf("{") > -1 || expression.indexOf("}") > -1) {
+
+            }
+        }
+    }
+}
