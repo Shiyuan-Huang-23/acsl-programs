11a1b398821208224310f8543c31a3f76aa09ccc
Shiyuan Huang <19438967+Grey16@users.noreply.github.com>
2018-02-17 20:47:55 -0500

    update enclosures ArrayList

--- junior/contests/Enclosure.java
+++ junior/contests/Enclosure.java
@@ -1,16 +1,26 @@
-//Java version of the Python program submitted for Contest 2
 import java.util.*;
 
 public class Enclosure {
     public static void main(String[] args) {
-        for(int i = 0; i < 5; i++) {
-            ArrayList<String> enclosures = new ArrayList<String>();
+        for(int i = 0; i < 1; i++) {
+            ArrayList<String> enclosures = new ArrayList<String>(Arrays.asList("{", "[", "(", ")", "]", "}"));
             Scanner in = new Scanner(System.in);
             System.out.print("Enter the mathematical expression without any spaces: ");
             String expression = in.nextLine();
-            if(expression.indexOf("{") > -1 || expression.indexOf("}") > -1) {
-
+            // update enclosures to reflect enclosures in the expression
+            if(expression.indexOf("{") < 0 && expression.indexOf("}") < 0) {
+                enclosures.remove("{");
+                enclosures.remove("}");
             }
+            if(expression.indexOf("[") < 0 && expression.indexOf("]") < 0) {
+                enclosures.remove("[");
+                enclosures.remove("]");
+            }
+            if(expression.indexOf("(") < 0 && expression.indexOf(")") < 0) {
+                enclosures.remove("(");
+                enclosures.remove(")");
+            }
+            System.out.println(enclosures);
         }
     }
 }
