6d9ab468307f959a11c19b62b86c6d3937ace430
Shiyuan Huang <19438967+Grey16@users.noreply.github.com>
2018-02-19 09:58:36 -0500

    fill enclosureIndices with indices of enclosures

--- junior/contests/Enclosure.java
+++ junior/contests/Enclosure.java
@@ -3,24 +3,38 @@ import java.util.*;
 public class Enclosure {
     public static void main(String[] args) {
         for(int i = 0; i < 1; i++) {
-            ArrayList<String> enclosures = new ArrayList<String>(Arrays.asList("{", "[", "(", ")", "]", "}"));
+            ArrayList<String> enclosuresList = new ArrayList<String>(Arrays.asList("{", "[", "(", ")", "]", "}"));
             Scanner in = new Scanner(System.in);
             System.out.print("Enter the mathematical expression without any spaces: ");
             String expression = in.nextLine();
             // update enclosures to reflect enclosures in the expression
             if(expression.indexOf("{") < 0 && expression.indexOf("}") < 0) {
-                enclosures.remove("{");
-                enclosures.remove("}");
+                enclosuresList.remove("{");
+                enclosuresList.remove("}");
             }
             if(expression.indexOf("[") < 0 && expression.indexOf("]") < 0) {
-                enclosures.remove("[");
-                enclosures.remove("]");
+                enclosuresList.remove("[");
+                enclosuresList.remove("]");
             }
             if(expression.indexOf("(") < 0 && expression.indexOf(")") < 0) {
-                enclosures.remove("(");
-                enclosures.remove(")");
+                enclosuresList.remove("(");
+                enclosuresList.remove(")");
             }
-            System.out.println(enclosures);
+            String[] enclosures = new String[enclosuresList.size()];
+            enclosures = enclosuresList.toArray(enclosures);
+
+            // fill enclosureIndices with the indices of the symbols in enclosures
+            String missingSymbol = "";
+            int missingSymbolIndex = -1;
+            int[] enclosureIndices = new int[enclosures.length];
+            for(int j = 0; j < enclosures.length; j++) {
+                enclosureIndices[j] = expression.indexOf(enclosures[j]);
+                if(enclosureIndices[j] == -1) {
+                    missingSymbol = enclosures[j];
+                    missingSymbolIndex = j;
+                }
+            }
+            System.out.println(missingSymbol);
         }
     }
 }
