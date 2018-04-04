9f6618cd421ed71f48fa5a323ac3a01cc6411772
Shiyuan Huang <19438967+Grey16@users.noreply.github.com>
2018-04-03 21:47:52 -0400

    Update Duplicates.java

--- junior/contests/Duplicates.java
+++ junior/contests/Duplicates.java
@@ -4,4 +4,9 @@ American Computer Science League
 Python and Java are programming languages
 Python and Java and java and python
 the quick brown fox jumped over the lazy river
+
+_____a!b#@r$aca%da^bra&ca*bo(b)----
+abcdefghijklmnopqrstuvwxyz
+aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ
+
 */
