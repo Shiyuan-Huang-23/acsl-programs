44d1447c29b9ccd8f3276dcb7d17a344142bba5d
Shiyuan Huang <19438967+Grey16@users.noreply.github.com>
2018-05-21 22:38:30 -0400

    Update Tomography.py

--- AllStars/Tomography.py
+++ AllStars/Tomography.py
@@ -5,24 +5,31 @@ def main():
             currIn = inArr[i].split(", ")
             numRows = len(currIn[0])
             numCols = len(currIn[1])
-            print(str(numRows) + " " + str(numCols))
+            # print(str(numRows) + " " + str(numCols))
             arr = [[1, 0], [0, 0]]
-            print(check(arr, "10", "10"))
+            # print(check(arr, "10", "10"))
             arr = list()
             for j in range(numRows):
                 curr = [None] * numCols
                 arr.append(curr)
-            print(recur(arr, currIn[0], currIn[1]))
+            result = recur(arr, currIn[0], currIn[1])
+            for row in result:
+                for cell in row:
+                    print(str(cell) + " ", end = "")
+                print()
 
 def recur(arr, rowSum, colSum):
     if not complete(arr):
         for j in range(len(arr)):
             for k in range(len(arr[0])):
-                for i in range(2):
-                    arr[j][k] = i
-                    result = recur(arr, rowSum, colSum)
-                    if result != False:
-                        return result
+                if arr[j][k] == None:
+                    for i in range(2):
+                        arr[j][k] = i
+                        result = recur(arr, rowSum, colSum)
+                        if result != False:
+                            return result
+                    arr[j][k] = None
+                    return False
     else:
         if(check(arr, rowSum, colSum)):
             return arr
@@ -49,3 +56,14 @@ def check(arr, rowSum, colSum):
     return True
 
 if __name__ == "__main__": main()
+
+# 10, 10
+# 20, 11
+# 21, 21
+# 100, 001
+# 300, 111
+# 031, 211
+# 102, 210
+# 111, 300
+# 2002, 2200
+# 2322, 4410
