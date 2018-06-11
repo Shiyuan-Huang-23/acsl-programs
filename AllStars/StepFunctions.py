c885002e1719c9cd00ba3fc1dd1dcef64791ba59
Shiyuan Huang <19438967+Grey16@users.noreply.github.com>
2018-06-11 16:50:27 -0400

    need to debug test inputs 2, 6-10

--- AllStars/StepFunctions.py
+++ AllStars/StepFunctions.py
@@ -42,8 +42,8 @@ def main():
                     seg.append(arr[index + 3])
                     index += 4
                 graph.append(seg)
-            for seg in graph:
-                print(seg)
+            # for seg in graph:
+            #     print(seg)
             # sorts graph segments so they are in descending order by y-values
             graph.sort(reverse = True)
             # modify this so extra spaces are added if there are extra conditions, including single digits "I"
@@ -55,17 +55,26 @@ def main():
             # these extra spaces should take into account the lengths of the segments on their lefts
 
             # all x-values that need to be printed
-            xVals = list()
+            xDict = {}
             for seg in graph:
-                if seg[2] not in xVals:
-                    xVals.append(seg[2])
+                if seg[2] not in xDict:
+                    xDict[seg[2]] = 1
+                else:
+                    xDict[seg[2]] += 1
                 if len(seg) == 4:
-                    if seg[3] not in xVals:
-                        xVals.append(seg[3])
+                    if seg[3] not in xDict:
+                        xDict[seg[3]] = 1
+                    else:
+                        xDict[seg[3]] += 1
+            # remove x values that don't occur in at least 2 segments
+            xVals = list()
+            for a, b in xDict.items():
+                if b >= 2:
+                    xVals.append(a)
             xVals.sort()
-            print(xVals)
+            # print(xVals)
 
-            # first look at ones that go to the left of the x value (A, D)
+            # first look at ones that go to the left of the x value (A, D, E, F, G, H)
             # then do the ones directly above x-value (I)
             # then ones to the right (B, C, E, F, G, H)
             # make sure there are no repeats
@@ -76,97 +85,37 @@ def main():
                 xCoord = xVals[j]
                 # if this is the first x-coord, also look at left
                 if j == 0:
-                    temp = segInd(graph, xCoord, ["<***O", "<***@"])
+                    temp = segInd(graph, xCoord, ["<***O", "<***@", "O***O", "@***@", "O***@", "@***O"], True)
                     if temp != -1:
                         graph[temp][1] = " " * numSegSpaces + graph[temp][1]
                         numSegSpaces += (len(graph[temp][1]) - 2)
                         numXSpaces = 6
                         xString = " " * numXSpaces + str(xCoord)
-                temp = segInd(graph, xCoord, ["@"])
+                temp = segInd(graph, xCoord, ["@"], False)
                 if temp != -1:
                     graph[temp][1] = " " * numSegSpaces + graph[temp][1]
                     if xCoord not in xString:
                         xString += " " * (len(graph[temp][1]) + 1 - len(xString)) + str(xCoord)
-                temp = segInd(graph, xCoord, ["O***>", "@***>", "O***O", "@***@", "O***@", "@***O"])
+                temp = segInd(graph, xCoord, ["O***>", "@***>", "O***O", "@***@", "O***@", "@***O"], True)
                 if temp != -1:
                     graph[temp][1] = " " * numSegSpaces + graph[temp][1]
                     numSegSpaces += len(graph[temp][1]) - 1
                     if xCoord not in xString:
                         xString += " " * (len(graph[temp][1]) + 1 - len(xString)) + str(xCoord)
+
+            # display graph
             for seg in graph:
                 print(seg[0] + seg[1])
-
-            # print(graph)
-            # print(xString)
-            # print(graph[0][1])
-            # print(graph[1][1])
             print(xString)
 
-                # look at directly above and to the right
-
-            # currLen = 0
-            # for j in range(len(xVals)):
-            #     temp = list()
-            #     for seg in graph:
-            #         if seg[2:] == xVals[j]:
-            #             temp.append(seg)
-            #     if len(temp) == 1:
-            #         for seg in graph:
-            #             if seg[2:] == xVals[j]:
-            #                 if j == 0:
-            #                     seg[1] = " " + seg[1]
-            #                 else:
-            #                     seg[1] = (" " * currLen) + seg[1]
-            #                 currLen = len(seg[1]) - 1
-            #                 seg.append(True)
-            #     else:
-            #         found = False
-            #         for line in temp:
-            #             if "<" in line[1]:
-            #                 for s in graph:
-            #                     if s == line:
-            #                         s.append(True)
-            #                         print(s)
-            #                         break
-            #                 if j == 0:
-            #                     seg[1] = " " + seg[1]
-            #                 else:
-            #                     seg[1] = (" " * currLen) + seg[1]
-            #                 currLen = len(seg[1]) - 1
-            #                 found = True
-            #         if not found:
-            #             for line in temp:
-            #                 if line[1] == "@":
-            #                     if j == 0:
-            #                         seg[1] = " " + seg[1]
-            #                     else:
-            #                         seg[1] = (" " * currLen) + seg[1]
-            #                     currLen = len(seg[1]) - 1
-            #                     found = True
-            #         if not found:
-            #             for line in temp:
-            #                 if ">" in line[1]:
-            #                     if j == 0:
-            #                         seg[1] = " " + seg[1]
-            #                     else:
-            #                         seg[1] = (" " * currLen) + seg[1]
-            #                     currLen = len(seg[1]) - 1
-            #         # seg.append(True)
-
-
-            # for seg in graph:
-            #     # prints y-value
-            #     print(seg[0], end = "")
-            #     print(seg[1])
-            # # modify this so it will print correctly even if there are multiple conditions
-            # # modify this so it will not print places where there are no overlaps or no undefineds
-            # print("******" + graph[0][2])
-
 # returns the index of the segment that contains the correct x-coord and the correct direction, -1 if not found
-def segInd(graph, xCoord, direction):
+def segInd(graph, xCoord, direction, last):
     for i in range(len(graph)):
         seg = graph[i]
-        if seg[2] == xCoord and seg[1] in direction:
+        index = 2
+        if last:
+            index = len(seg) - 1
+        if seg[index] == xCoord and seg[1] in direction:
             return i
     return -1
 
@@ -174,7 +123,19 @@ if __name__ == "__main__": main()
 
 # Sample Input
 # 2, 0, A, 2, 1, C, 2
-# 2, 0, B, 1, 1, A, 1
+# 2, 0, A, 1, 1, B, 1
 # 2, 0, B, 2, 1, D, 2
 # 3, 1, A, 0, 2, I, 0, 3, B, 0
 # 2, 1, F, 0, 3, 0, B, 3
+
+# Test Input
+# 2, 2, D, 3, 1, B, 3
+# 2, 2, F, 3, 4, 1, E, 2, 3 debug
+# 2, 2, A, 4, 1, C, 4
+# 3, 3, B, 5, 2, I, 5, 1, A, 5
+# 2, 3, B, 4, 1, A, 4
+# 3, 4, E, 5, 7, 3, I, 5, 2, H, -2, 5 debug
+# 3, 4, I, 4, 3, I, 3, 2, I, 2 debug
+# 3, 4, F, 5, 6, 3, H, 4, 5, 2, H, 3, 4 debug
+# 3, 4, B, 5, 3, E, 4, 5, 2, A, 4 debug
+# 3, 4, C, 6, 3, H, 4, 6, 2, A, 4 debug
