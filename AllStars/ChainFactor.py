66d3201ceda6fae1926b941ffcb2b13a8be9709e
Shiyuan Huang <19438967+Grey16@users.noreply.github.com>
2018-06-23 09:30:45 -0400

    handle first drop

--- AllStars/ChainFactor.py
+++ AllStars/ChainFactor.py
@@ -6,13 +6,23 @@ def main():
         grid = [[0] * 7 for i in range(7)]
         for i in range(len(myInput)):
             currInput = myInput[i].split(", ")
-            if i == 0:
-                for j in range(len(currInput)):
+            for j in range(len(currInput)):
+                if i == 0:
                     if currInput[j] != "0":
                         for c in range(len(currInput[j])):
                             grid[6 - c][j] = currInput[j][c]
-        for row in grid:
-            print(row)
+                elif i == 1:
+                    grid = placeDisk(grid, currInput[j])
+
+            for row in grid:
+                print(row)
+
+def placeDisk(grid, coords):
+    c = ord(coords[0]) - 65
+    for r in range(6, -1, -1):
+        if grid[r][c] == 0:
+            grid[r][c] = int(coords[1])
+            return grid
 
 main()
 # 436, 454, 2, 0, 67, 0, 0
