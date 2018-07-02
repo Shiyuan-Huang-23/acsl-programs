e66e5db2e6f303f235873bba1ea299a331128898
Shiyuan Huang <19438967+Grey16@users.noreply.github.com>
2018-07-02 13:39:01 -0400

    tested sample input

--- AllStars/Sticklinks.py
+++ AllStars/Sticklinks.py
@@ -10,10 +10,57 @@ def main():
                 currInput = currInput.split(", ")
                 for j in range(len(currInput)):
                     grid.append(list(currInput[len(currInput) - j - 1]))
-                for row in grid:
-                    print(row)
-                print(getCoords(grid, 3, 0))
-                print(getAdj(grid, 4, 0))
+                # for row in grid:
+                #     print(row)
+                # print(getCoords(grid, 3, 0))
+                # print(getAdj(grid, 4, 0))
+            else:
+                result = findWord(grid, currInput, None)
+                if len(result) > 0:
+                    for n in result:
+                        print(n[0], end = "")
+                        print("," + str(n[1]) + "  ", end = "")
+                    print()
+                else:
+                    print("NOT THERE")
+
+def findWord(grid, word, coords):
+    result = []
+    if coords == None:
+        for r in range(len(grid)):
+            for c in range(len(grid)):
+                if grid[r][c] == word[0]:
+                    result.append(getCoords(grid, r, c))
+                    if len(word) > 1:
+                        recur = findWord(grid, word[1:], [r, c])
+                        if len(recur) > 0:
+                            result += recur
+                            if len(result) == len(word):
+                                return result
+                        else:
+                            result = []
+                    else:
+                        if len(result) == len(word):
+                            return result
+    else:
+        r = coords[0]
+        c = coords[1]
+        searchArea = getAdj(grid, r, c)
+        for n in searchArea:
+            if grid[n[0]][n[1]] == word[0]:
+                result.append(getCoords(grid, n[0], n[1]))
+                if len(word) > 1:
+                    recur = findWord(grid, word[1:], [n[0], n[1]])
+                    if len(recur) > 0:
+                        result += recur
+                        if len(result) == len(word):
+                            return result
+                    else:
+                        result = []
+                else:
+                    if len(result) == len(word):
+                            return result
+    return result
 
 def getAdj(grid, r, c):
     coords = []
