52efe24348566033d64bb80980928dfee6ae85f4
Shiyuan Huang <19438967+Grey16@users.noreply.github.com>
2018-07-06 13:58:48 -0400

    count points in each hand

--- AllStars/BridgeBidding.py
+++ AllStars/BridgeBidding.py
@@ -5,6 +5,23 @@ def main():
         myInput = [line.rstrip() for line in f]
         for i in range(len(myInput)):
             currInput = myInput[i].split(", ")
+            points = countPoints(currInput[:-1])
+
+def countPoints(hand):
+    points = 0
+    for suit in hand:
+        for card in suit:
+            if card == "A":
+                points += 4
+            elif card == "K":
+                points += 3
+            elif card == "Q":
+                points += 2
+            elif card == "J":
+                points += 1
+            else:
+                break
+    return points
 
 main()
 
