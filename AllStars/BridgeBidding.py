d40731133071fa445096d66d5693b35c13f8cc69
Shiyuan Huang <19438967+Grey16@users.noreply.github.com>
2018-07-07 14:23:18 -0400

    tried sample input, need to do test input

--- AllStars/BridgeBidding.py
+++ AllStars/BridgeBidding.py
@@ -1,11 +1,82 @@
 import math
 
 def main():
+    suits = ["S", "H", "D", "C"]
     with open("BridgeBidding.txt") as f:
         myInput = [line.rstrip() for line in f]
+        num = 1
         for i in range(len(myInput)):
             currInput = myInput[i].split(", ")
             points = countPoints(currInput[:-1])
+            bid = ""
+            if points >= 13:
+                if len(currInput[0]) >= 5:
+                    bid = "1S"
+                elif len(currInput[1]) >= 5:
+                    bid = "1H"
+                elif len(currInput[2]) >= 3:
+                    bid = "1D"
+                elif len(currInput[3]) >= 3:
+                    bid = "1C"
+            else:
+                bid = "PASS"
+            print(str(num) + ". " + bid)
+            num += 1
+            openBid = currInput[4]
+            suit = openBid[1]
+            bid = ""
+            if points >= 10:
+                if suit == "S" and len(currInput[0]) >= 3:
+                    bid = "3S"
+                elif suit == "H" and len(currInput[1]) >= 3:
+                    bid = "3H"
+                elif suit == "D" and len(currInput[2]) >= 4:
+                    bid = "3D"
+                elif suit == "C" and len(currInput[3]) >= 4:
+                    bid = "3C"
+                if points >= 15:
+                    for s in range(len(suits)):
+                        if s != suits.index(suit):
+                            if len(currInput[s]) >= 6:
+                                bid = "3" + suits[s]
+                                break
+                if bid == "":
+                    for s in range(len(suits)):
+                        if s != suits.index(suit):
+                            if s == 0 and len(currInput[s]) >= 5:
+                                bid = "2S"
+                                break
+                            elif s == 1 and len(currInput[s]) >= 5:
+                                bid = "2H"
+                                break
+                            elif s == 2 and len(currInput[s]) >= 4:
+                                bid = "2D"
+                                break
+                            elif s == 3 and len(currInput[s]) >= 4:
+                                bid = "2C"
+                                break
+                if bid == "":
+                    bid = "2 NO TRUMP"
+            elif points >= 6 and points <= 9:
+                if suit == "S" and len(currInput[0]) >= 3:
+                    bid = "2S"
+                elif suit == "H" and len(currInput[1]) >= 3:
+                    bid = "2H"
+                elif suit == "D" and len(currInput[2]) >= 4:
+                    bid = "2D"
+                elif suit == "C" and len(currInput[3]) >= 4:
+                    bid = "2C"
+                if bid == "":
+                    for s in range(len(suits)):
+                        if s < suits.index(suit) and len(currInput[s]) >= 4:
+                            bid = "1" + suits[s]
+                            break
+                if bid == "":
+                    bid = "1 NO TRUMP"
+            elif points < 6:
+                bid = "PASS"
+            print(str(num) + ". " + bid)
+            num += 1
 
 def countPoints(hand):
     points = 0
